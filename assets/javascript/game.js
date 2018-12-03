fe2b058ebd9a4eba312ee9cbf396e81821fb02a0
sharitacumm <emandraine@yahoo.com>
2018-12-03 07:17:56 -0500

    added buttons for stones

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,2 +1,9 @@
 console.log("This is working");
-             
+
+// code for random number box//
+$(document).ready(function(){
+    var targetNumber=Mathfloor(Math.random()*(102)+19;
+
+    $("randomNumberBox").text(targetrNUmber);
+
+});
